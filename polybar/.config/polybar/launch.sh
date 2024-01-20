9a3090a48c1663a22345fb27b2706a9760885ab7
Michael McAllister <git@sknk.ws>
2024-01-20 18:46:09 +1100

    Migrate to TOML for Alacritty

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar top &
+MONITOR="LVDS-1" polybar top &
+MONITOR="VNC-0" polybar top &
 
 echo "Polybar launched..."
